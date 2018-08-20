5135e71283659364a7c2985f4d0e6b8f2c12a549
Solange Umuhire GASENGAYIRE <sgasengayire@gmail.com>
2018-08-20 10:56:33 +0200

    Implement handlers POST methods, and fix article retrieval from memcache

--- wiki/article.py
+++ wiki/article.py
@@ -1,3 +1,4 @@
+import re
 import time
 import logging
 import util.security as security
@@ -14,11 +15,9 @@ class ArticleHandler(handler.TemplateHandler):
     ArticleHandler inherits from the handler.TemplateHandler class.
     It aggregates functionalities for retrieving or creating a requested resource by the user.
     """
-    def render_article(self, logged=False, username=None, editionmode=False,
-                       title="", content="", author="", error=""):
+    def render_article(self, logged=False, username=None, editionmode=False, article=None, error=""):
         self.render("article.html", logged=logged, username=username,
-                    editionmode=editionmode, title=title, content=content, author=author,
-                    error=error)
+                    editionmode=editionmode, article=article, error=error)
 
     def get(self, title):
         # do we have a logged in user?
@@ -34,10 +33,9 @@ class ArticleHandler(handler.TemplateHandler):
         if title:
             article = get_article(title)
             if article:
-                self.render_article(logged=logged, username=username, title=article.title,
-                                    content=article.content, author=article.author)
+                self.render_article(logged=logged, username=username, article=article)
             else:
-                self.redirect('/_edit/%s' % title)
+                self.redirect('/_edit/%s' % title.replace(" ", "-"))
 
 
 class NewArticleHandler(handler.TemplateHandler):
@@ -45,11 +43,11 @@ class NewArticleHandler(handler.TemplateHandler):
     NewArticleHandler inherits from the hander.TemplateHandler class.
     It aggregates functionalities for creating and persisting new wiki articles to the datastore.
     """
-    def render_article(self, logged=False, username=None, editionmode=False,
-                       title="", content= "", author="", error=""):
-        self.render("article.html", logged=logged, username=username,
-                    editionmode=editionmode, title=title, content=content, author=author,
-                    error=error)
+    new_article = None
+
+    def render_article(self, logged=False, username=None, editionmode=False, article=None, error="", newarticle=False):
+        self.render("article.html", logged=logged, username=username, editionmode=editionmode, article=article,
+                    error=error, newarticle=newarticle)
 
     def get(self):
         # do we have a logged in user?
@@ -58,7 +56,10 @@ class NewArticleHandler(handler.TemplateHandler):
             username = security.check_secure_val(user_cookie)
             if username:
                 # a known user is logged in
-                self.render_article(logged=True, username=username, editionmode=True)
+                global new_article
+                new_article = Article(title=" ", content=" ", author=username)
+                self.render_article(logged=True, username=username, editionmode=True,
+                                    article=new_article, newarticle=True)
             else:
                 # only logged in users are allowed to create/edit articles
                 self.redirect('/login?from=%s' % self.request.url)
@@ -67,8 +68,40 @@ class NewArticleHandler(handler.TemplateHandler):
             self.redirect('/login?from=%s' % self.request.url)
 
     def post(self):
-        # TODO: implement method
-        pass
+        # retrieve the user currently logged in
+        user_cookie = self.request.cookies.get('user')
+
+        if user_cookie:
+            username = security.check_secure_val(user_cookie)
+            if username:
+                title = self.request.get('title')
+                content = self.request.get('content')
+
+                if title and content:
+                    new_article = Article(title=title, content=content, author=username)
+                    new_article.put()
+
+                    # rerun the DB query and update the cache
+                    get_top_articles(True)
+
+                    self.redirect('/wiki/%s' % title.replace(" ", "-"))
+                else:
+                    # all mandatory fields haven't been filled
+                    global new_article
+                    if title:
+                        new_article.title = title
+                    if content:
+                        new_article.content = content
+
+                    error = "We need both a title and valid non-empty content!"
+                    self.render_article(logged=True, username=username, editionmode=True,
+                                        article=new_article, error=error, newarticle=True)
+            else:
+                # in case we don't have a valid username, redirect to login page
+                self.redirect('/login?from=%s' % self.request.url)
+        else:
+            # in case we don't have a valid cookie
+            self.redirect('/login?from=%s' % self.request.url)
 
 
 class ArticleEditorHandler(handler.TemplateHandler):
@@ -76,11 +109,11 @@ class ArticleEditorHandler(handler.TemplateHandler):
     ArticleEditorHandler inherits from the hander.TemplateHandler class.
     It aggregates functionalities for creating, editing or persisting wiki articles to the datastore.
     """
-    def render_article(self, logged=False, username=None, editionmode=False,
-                       title="", content= "", author="", error=""):
-        self.render("article.html", logged=logged, username=username,
-                    editionmode=editionmode, title=title, content=content, author=author,
-                    error=error)
+    known_article = None
+
+    def render_article(self, logged=False, username=None, editionmode=False, article=None, error="", newarticle=False):
+        self.render("article.html", logged=logged, username=username, editionmode=editionmode, article=article,
+                    error=error, newarticle=newarticle)
 
     def get(self, article_title):
         username = None
@@ -93,28 +126,66 @@ class ArticleEditorHandler(handler.TemplateHandler):
             if username:
                 # a known user is logged in
                 # has he requested a particular article?
-                title = article_title
+                title = article_title.replace("-", " ")
                 if title:
-                    article = get_article(title)
-                    if article:
+                    global known_article
+                    known_article = get_article(title)
+                    if known_article:
                         # edition of an already existing article with the input title
                         self.render_article(logged=True, username=username, editionmode=True,
-                                            title=title, content=article.content, author=article.author)
+                                            article=known_article)
                     else:
                         # creation of a new article with the presubmitted title
+                        known_article = Article(title=title, content=" ", author=username)
                         self.render_article(logged=True, username=username, editionmode=True,
-                                            title=title)
+                                            article=known_article, newarticle=True)
                 else:
                     # creation of a new article without a presubmitted title
-                    self.render_article(logged=True, username=username, editionmode=True)
+                    self.render_article(logged=True, username=username, editionmode=True, article=known_article)
 
         if not username:
             # only logged in users are allowed to create/edit articles
             self.redirect('/login?from=%s' % self.request.url)
 
-    def post(self):
-        # TODO: implement method
-        pass
+    def post(self, article_title):
+        # retrieve the user currently logged in
+        user_cookie = self.request.cookies.get('user')
+
+        if user_cookie:
+            username = security.check_secure_val(user_cookie)
+            if username:
+                title = self.request.get('title')
+                content = self.request.get('content')
+                if not title:
+                    title = article_title
+
+                global known_article
+
+                if title and content:
+                    # update properties of edited article before saving
+                    known_article.title = title
+                    known_article.content = content
+                    known_article.author = username
+                    known_article.put()
+
+                    # rerun the DB query and update the cache
+                    get_top_articles(True)
+
+                    self.redirect('/wiki/%s' % title)
+                else:
+                    if title:
+                        known_article.title = title
+                    if content:
+                        known_article.content = content
+                    error = "We need both a title and valid non-empty content!"
+                    self.render_article(logged=True, username=username, editionmode=True,
+                                        article=known_article, error=error)
+            else:
+                # in case we don't have a valid username, redirect to login page
+                self.redirect('/login?from=%s' % self.request.url)
+        else:
+            # in case we don't have a valid cookie
+            self.redirect('/login?from=%s' % self.request.url)
 
 
 def get_article(title):
@@ -128,7 +199,10 @@ def get_article(title):
     articles = memcache.get('top_ten')
     if articles and len(articles) > 0:
         for item in articles:
-            if item.title == title:
+            # workaround to remove all non-alphanumeric characters before comparison
+            item_title = re.sub(r'\W', "", item.title)
+            art_title = re.sub(r'\W', "", title)
+            if item_title == art_title:
                 article = item
                 break
     # in case the article we're looking for is not in memcache:
